418a9bc0678fedf845c0d4fc019b5e1ab971c5ce
sriram <sriramsridharjob78@gmail.com>
2019-04-09 08:13:18 -0400

    Trying to make the grok filter work

--- plotter/plotter.py
+++ plotter/plotter.py
@@ -110,7 +110,7 @@ class Plotter:
                     current_retry_attempts = 0
                     latency_list = eval(latency_list[0].decode('utf-8'))
                     for latency in latency_list:
-                        logging.info("{} : {}".format(dt, latency))
+                        logging.info("Sent_timestamp:{},Latency_in_milliseconds:{}".format(dt, latency))
 
                 year = ts.year
                 month = ts.month
