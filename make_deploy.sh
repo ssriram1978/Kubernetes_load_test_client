b483b2fbb0a09c6d13e3f12de1feb5144789872b
Sriram Sridhar <sriramsridhar@C02Y75JAJGH5.local>
2019-04-11 09:54:51 -0400

    Fixed the make deploy script to make and deploy publisher, subscriber, plotter, logstash.

--- make_deploy.sh
+++ make_deploy.sh
@@ -20,14 +20,15 @@ build_push_directory() {
    echo "cp infrastructure_components.tar.gz $directory_name/"
    cp infrastructure_components.tar.gz $directory_name
 
-   if [ "$2" != "" ]; then
-      echo "docker build $directory_name -t $tag/$directory_name:latest"
-      docker build $directory_name -t $tag/$directory_name:latest
-     echo "docker push $tag/$directory_name:latest"
-     docker push $tag/$directory_name:latest
+   if [[ "$2" != "" ]]; then
+        echo "docker build $directory_name -t $tag/$directory_name:latest"
+        docker build $directory_name -t $tag/$directory_name:latest
+
+        echo "docker push $tag/$directory_name:latest"
+        docker push $tag/$directory_name:latest
    else
-      echo "docker build $directory_name -t $directory_name:latest"
-      docker build $directory_name -t $directory_name:latest
+        echo "docker build $directory_name -t $directory_name:latest"
+        docker build $directory_name -t $directory_name:latest
    fi
 
    echo "rm -f $directory_name/infrastructure_components.tar.gz"
@@ -37,39 +38,43 @@ build_push_directory() {
 
 create_infrastructure() {
    directory_name=$1
-   tag=$2
+   yaml_file=$2
+   tag=$3
 
    echo "gzip_infrastructure_components"
    gzip_infrastructure_components
 
-   if [ "$1" == "all" ]; then
-    echo "build_push_directory publisher $tag"
-    build_push_directory \
-      publisher \
-      $tag
-    echo "build_push_directory subscriber $tag"
-    build_push_directory \
-      subscriber \
-      $tag
-    echo "build_push_directory subscriber $tag"
-    build_push_directory \
-      plotter \
-      $tag
-    echo "build_push_directory subscriber $tag"
-    build_push_directory \
-        plotter/logstash \
+   if [[ "$1" == "all" ]]; then
+        echo "build_push_directory publisher $tag"
+        build_push_directory \
+        publisher \
         $tag
+
+        echo "build_push_directory subscriber $tag"
+        build_push_directory \
+        subscriber \
+        $tag
+
+        echo "build_push_directory plotter $tag"
+        build_push_directory \
+        plotter \
+        $tag
+
+        echo "docker-compose -f $3 build"
+        docker-compose -f $3 build
     else
-      echo "build_push_directory publisher $tag"
-      build_push_directory \
+        echo "build_push_directory publisher $tag"
+        build_push_directory \
         $1 \
         $tag
     fi
 }
 
 deploy_infrastructure() {
-   echo "docker stack deploy -c docker-compose.yml load_test"
-   docker stack deploy -c docker-stack.yml load_test
+   yaml_file=$1
+
+   echo "docker stack deploy -c $1 load_test"
+   docker stack deploy -c $1 load_test
 }
 
 teardown_infrastructure() {
@@ -79,23 +84,30 @@ teardown_infrastructure() {
 
 create_deploy_infrastructure() {
    directory_name=$1
-   tag=$2
+   yaml_file=$2
+   tag=$3
 
    echo "create_infrastructure "
    create_infrastructure \
      $directory_name \
+     $yaml_file \
      $tag
 
    echo "deploy_infrastructure"
-   deploy_infrastructure
+   deploy_infrastructure \
+      $yaml_file
 }
 
 case "$1" in
-  build) create_infrastructure $2 $3 ;;
-  deploy) deploy_infrastructure ;;
-  build_and_deploy) create_deploy_infrastructure $2 $3 ;;
-  stop) teardown_infrastructure ;;
-  *) echo "usage: $0 <build <all|directory_name> <tag>> | <deploy> | <build_and_deploy <all|directory_name> <tag>> |stop"
+  build) create_infrastructure $2 $3 $4 ;;
+  deploy) deploy_infrastructure $2 ;;
+  build_and_deploy) create_deploy_infrastructure $2 $3 $4 ;;
+  stop) teardown_infrastructure  ;;
+  *) echo "usage: $0"
+      echo "<build <all|directory_name> <yaml file> <tag -- optional> > |"
+      echo "<deploy <yaml file> > |"
+      echo "<build_and_deploy <all|directory_name> <yaml file> <tag --optional>> | "
+      echo "stop"
      exit 1
      ;;
 esac
