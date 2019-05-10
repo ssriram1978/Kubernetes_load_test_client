ef6ea72f6012bb50edc5beef6407029a44ef2a68
Sriram Sridhar <sriram@charlie.verizon.com>
2019-05-10 18:01:30 +0000

    Add sudo to the systcl invocations. Move the filebeat chmod and chown commands into the make deploy function for it to get invoked when the load test comes up.

--- make_deploy.sh
+++ make_deploy.sh
@@ -112,12 +112,6 @@ monitor_infrastructure() {
         echo "sysctl_tcp_kernel_optimization"
         sysctl_tcp_kernel_optimization
 
-       echo "chmod go-w plotter/filebeat/filebeat.docker.yml"
-       chmod go-w plotter/filebeat/filebeat.docker.yml
-
-       echo "chown root:root plotter/filebeat/filebeat.docker.yml"
-       chown root:root plotter/filebeat/filebeat.docker.yml
-
        echo "docker-compose -f $yaml_file build"
        docker-compose -f ${yaml_file} build
 
@@ -136,6 +130,12 @@ deploy_infrastructure() {
 
    echo "sysctl_tcp_kernel_optimization"
    sysctl_tcp_kernel_optimization
+
+  echo "chmod go-w plotter/filebeat/filebeat.docker.yml"
+  sudo chmod go-w plotter/filebeat/filebeat.docker.yml
+
+   echo "chown root:root plotter/filebeat/filebeat.docker.yml"
+   sudo chown root:root plotter/filebeat/filebeat.docker.yml
 
    echo "docker stack deploy --compose-file docker-stack-common.yml -c $yaml_file $tag"
    docker stack deploy --compose-file docker-stack-common.yml  -c $yaml_file $tag
@@ -160,105 +160,105 @@ docker_prune() {
 sysctl_tcp_kernel_optimization() {
 
     echo "fs.file-max=2097152 >> /etc/sysctl.conf"
-    echo "fs.file-max=2097152" >> /etc/sysctl.conf
+    sudo echo "fs.file-max=2097152" >> /etc/sysctl.conf
 
     echo "fs.nr_open=2097152  >> /etc/sysctl.conf"
-    echo "fs.nr_open=2097152" >> /etc/sysctl.conf
+    sudo echo "fs.nr_open=2097152" >> /etc/sysctl.conf
 
     echo "net.ipv4.conf.default.rp_filter=0 >> /etc/sysctl.conf"
-    echo "net.ipv4.conf.default.rp_filter=0" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.conf.default.rp_filter=0" >> /etc/sysctl.conf
 
     echo "net.core.somaxconn=32768  >> /etc/sysctl.conf"
-    echo "net.core.somaxconn=32768" >> /etc/sysctl.conf
+    sudo echo "net.core.somaxconn=32768" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_max_syn_backlog=20480  >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_max_syn_backlog=20480" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_max_syn_backlog=20480" >> /etc/sysctl.conf
 
     echo "net.core.netdev_max_backlog=16384  >> /etc/sysctl.conf"
-    echo "net.core.netdev_max_backlog=16384" >> /etc/sysctl.conf
+    sudo echo "net.core.netdev_max_backlog=16384" >> /etc/sysctl.conf
 
     echo "net.ipv4.ip_local_port_range=1024 65535  >> /etc/sysctl.conf"
-    echo "net.ipv4.ip_local_port_range=1024 65535" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.ip_local_port_range=1024 65535" >> /etc/sysctl.conf
 
     echo "net.core.wmem_default=12582912  >> /etc/sysctl.conf"
-    echo "net.core.wmem_default=12582912" >> /etc/sysctl.conf
+    sudo echo "net.core.wmem_default=12582912" >> /etc/sysctl.conf
 
     echo "net.core.wmem_default=12582912  >> /etc/sysctl.conf"
-    echo "net.core.wmem_default=12582912" >> /etc/sysctl.conf
+    sudo echo "net.core.wmem_default=12582912" >> /etc/sysctl.conf
 
     echo "net.core.rmem_max=16777216  >> /etc/sysctl.conf"
-    echo "net.core.rmem_max=16777216" >> /etc/sysctl.conf
+    sudo echo "net.core.rmem_max=16777216" >> /etc/sysctl.conf
 
     echo "net.core.wmem_max=16777216 >> /etc/sysctl.conf"
-    echo "net.core.wmem_max=16777216" >> /etc/sysctl.conf
+    sudo echo "net.core.wmem_max=16777216" >> /etc/sysctl.conf
 
     echo "net.core.optmem_max=16777216 >> /etc/sysctl.conf"
-    echo "net.core.optmem_max=16777216" >> /etc/sysctl.conf
+    sudo echo "net.core.optmem_max=16777216" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_rmem=10240 87380 16777216 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_rmem=10240 87380 16777216" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_rmem=10240 87380 16777216" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_wmem=10240 87380 16777216 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_wmem=10240 87380 16777216" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_wmem=10240 87380 16777216" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_max_tw_buckets=400000 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_max_tw_buckets=400000" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_max_tw_buckets=400000" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_fin_timeout=15 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_fin_timeout=15" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_fin_timeout=15" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_keepalive_time=30 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_keepalive_time=30" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_keepalive_time=30" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_keepalive_intvl=15 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_keepalive_intvl=15" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_keepalive_intvl=15" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_keepalive_probes=4 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_keepalive_probes=4" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_keepalive_probes=4" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_mem=8388608 8388608 8388608 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_mem=8388608 8388608 8388608" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_mem=8388608 8388608 8388608" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_window_scaling=1 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_window_scaling=1" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_window_scaling=1" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_tw_reuse=1 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_tw_reuse=1" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_tw_reuse=1" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_max_tw_buckets=400000 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_max_tw_buckets=400000" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_max_tw_buckets=400000" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_no_metrics_save=1 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_no_metrics_save=1" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_no_metrics_save=1" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_syn_retries=2 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_syn_retries=2" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_syn_retries=2" >> /etc/sysctl.conf
 
     echo "net.ipv4.tcp_synack_retries=2 >> /etc/sysctl.conf"
-    echo "net.ipv4.tcp_synack_retries=2" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.tcp_synack_retries=2" >> /etc/sysctl.conf
 
     # Connection tracking to prevent dropped connections (usually issue on LBs)
     echo "net.netfilter.nf_conntrack_max=262144 >> /etc/sysctl.conf"
-    echo "net.netfilter.nf_conntrack_max=262144" >> /etc/sysctl.conf
+    sudo echo "net.netfilter.nf_conntrack_max=262144" >> /etc/sysctl.conf
 
     echo "net.netfilter.nf_conntrack_generic_timeout=120 >> /etc/sysctl.conf"
-    echo "net.netfilter.nf_conntrack_generic_timeout=120" >> /etc/sysctl.conf
+    sudo echo "net.netfilter.nf_conntrack_generic_timeout=120" >> /etc/sysctl.conf
 
     echo "net.netfilter.nf_conntrack_tcp_timeout_established=86400 >> /etc/sysctl.conf"
-    echo "net.netfilter.nf_conntrack_tcp_timeout_established=86400" >> /etc/sysctl.conf
+    sudo echo "net.netfilter.nf_conntrack_tcp_timeout_established=86400" >> /etc/sysctl.conf
 
     # ARP cache settings for a highly loaded docker swarm
     echo "net.ipv4.neigh.default.gc_thresh1=8096 >> /etc/sysctl.conf"
-    echo "net.ipv4.neigh.default.gc_thresh1=8096" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.neigh.default.gc_thresh1=8096" >> /etc/sysctl.conf
 
     echo "net.ipv4.neigh.default.gc_thresh2=12288 >> /etc/sysctl.conf"
-    echo "net.ipv4.neigh.default.gc_thresh2=12288" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.neigh.default.gc_thresh2=12288" >> /etc/sysctl.conf
 
     echo "net.ipv4.neigh.default.gc_thresh3=16384 >> /etc/sysctl.conf"
-    echo "net.ipv4.neigh.default.gc_thresh3=16384" >> /etc/sysctl.conf
+    sudo echo "net.ipv4.neigh.default.gc_thresh3=16384" >> /etc/sysctl.conf
 
     echo "sysctl -p"
-    sysctl -p
+    sudo sysctl -p
 
 }
 
