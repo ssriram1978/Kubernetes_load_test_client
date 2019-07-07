b3bc3e89c65a6eaa91df1044aa747660532c32c2
Sriram Sridhar <sriramsridharjob78@gmail.com>
2019-07-07 15:21:39 -0400

    Made the publisher and subscriber golang files compute end-to-end latency.

--- go/src/publisher/publisher.go
+++ go/src/publisher/publisher.go
@@ -3,40 +3,69 @@ package main
 import (
 	"fmt"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	"time"
+	"encoding/json"
+	"os"
 )
 
 func main() {
 
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "192.168.1.92:9200"})
+	topic := os.Getenv("publisher_topic")
+	kafka_broker := os.Getenv("broker_hostname_key")
+	kafka_broker_port := os.Getenv("broker_port_key")
+	fmt.Printf("Topic=%s,broker=%s,port=%s\n",topic,kafka_broker,kafka_broker_port)
+
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": kafka_broker})
 	if err != nil {
 		panic(err)
 	}
+        fmt.Printf("Created Producer %v\n", p)
 
-	defer p.Close()
+	doneChan := make(chan bool)
 
-	// Delivery report handler for produced messages
+	// Produce messages to topic (asynchronously)
+
+	type SensorReading struct {
+		Date  string
+		Data string
+	}
+
 	go func() {
+		defer close(doneChan)
 		for e := range p.Events() {
 			switch ev := e.(type) {
 			case *kafka.Message:
-				if ev.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				m := ev
+				if m.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
 				} else {
-					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+					fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+						*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
 				}
+				return
+
+			default:
+				fmt.Printf("Ignored event: %s\n", ev)
 			}
 		}
 	}()
-
-	// Produce messages to topic (asynchronously)
-	topic := "PUBLISHER2"
-	for _, word := range []string{"Welcome", "to", "the", "Confluent", "Kafka", "Golang", "client"} {
-		fmt.Printf("Delivering: %v\n", word) 
-		p.Produce(&kafka.Message{
-			TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-			Value:          []byte(word),
-		}, nil)
+       for  {
+	       t := time.Now().Format(time.RFC3339Nano)
+		sensor := SensorReading{
+			Date: t,
+			Data: "ssss",
+		}
+		b, err := json.Marshal(sensor)
+		if err != nil {
+			fmt.Println("error:", err)
+		}	
+        	fmt.Printf("Delivering: %v\n", string(b)) 
+		p.ProduceChannel() <- &kafka.Message{TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny}, Value: []byte(b)}
 	}
+	// wait for delivery report goroutine to finish
+	_ = <-doneChan
+
+	p.Close()
 
 	// Wait for message deliveries before shutting down
 	p.Flush(15 * 1000)
--- go/src/subscriber/subscriber.go
+++ go/src/subscriber/subscriber.go
@@ -3,27 +3,51 @@ package main
 import (
 	"fmt"
 	"gopkg.in/confluentinc/confluent-kafka-go.v1/kafka"
+	"encoding/json"
+	"time"
+	"os"
 )
 
 func main() {
+        topic := os.Getenv("subscriber_topic")
+        kafka_broker := os.Getenv("broker_hostname_key")
+        kafka_broker_port := os.Getenv("broker_port_key")
+        fmt.Printf("Topic=%s,broker=%s,port=%s\n",topic,kafka_broker,kafka_broker_port)
 
 	c, err := kafka.NewConsumer(&kafka.ConfigMap{
-		"bootstrap.servers": "192.168.1.92:9092",
+		"bootstrap.servers": kafka_broker,
 		"group.id":          "subscriber-golang-group",
-		"auto.offset.reset": "earliest",
+		"auto.offset.reset": "latest",
 	})
 
 	if err != nil {
 		panic(err)
 	}
 
-	c.SubscribeTopics([]string{"PUBLISHER2", "^aRegex.PUBLISHER2"}, nil)
-	//c.SubscribeTopics([]string{"PUBLISHER3", "*"}, nil)
+        type SensorReading struct {
+                Date  string
+                Data string
+        }
+
+	c.SubscribeTopics([]string{topic, "^aRegex."}, nil)
 
 	for {
 		msg, err := c.ReadMessage(-1)
 		if err == nil {
-			fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+		        var sensor_reading SensorReading
+			//fmt.Printf("Message on %s: %s\n", msg.TopicPartition, string(msg.Value))
+			err := json.Unmarshal(msg.Value, &sensor_reading)
+			if err != nil {
+				fmt.Printf("error:", err)
+			}
+			//fmt.Printf("sensor_reading=%+v", sensor_reading)
+			t1, err := time.Parse(
+        			time.RFC3339Nano,
+        			sensor_reading.Date)
+			if err == nil  {
+				elapsed := time.Since(t1)
+				fmt.Printf("Latency=%s\n",elapsed)
+			}
 		} else {
 			// The client will automatically try to recover from all errors.
 			fmt.Printf("Consumer error: %v (%v)\n", err, msg)
