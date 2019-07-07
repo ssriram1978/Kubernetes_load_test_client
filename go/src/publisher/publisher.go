750757faf5de3c7869e0d1ca712a58094bf8bd78
Sriram Sridhar <sriramsridharjob78@gmail.com>
2019-07-07 18:38:58 -0400

    Finally made one instance of golang confluent kafka push 11k messages per second to kafka broker.

--- go/src/publisher/publisher.go
+++ go/src/publisher/publisher.go
@@ -19,13 +19,12 @@ func main() {
             [{"characteristicsName": "temperature","currentValue": "30.2999","readLevel":
             "R","parameterType": "Number","measurementUnit": "Celcius"}]}}`
 
-	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": kafka_broker})
+	    p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": kafka_broker, "acks": 0})
 	if err != nil {
 		panic(err)
 	}
         fmt.Printf("Created Producer %v\n", p)
 
-	doneChan := make(chan bool)
 
 	// Produce messages to topic (asynchronously)
 
@@ -34,26 +33,7 @@ func main() {
 		Data string
 	}
 
-	go func() {
-		defer close(doneChan)
-		for e := range p.Events() {
-			switch ev := e.(type) {
-			case *kafka.Message:
-				m := ev
-				if m.TopicPartition.Error != nil {
-					fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
-				} else {
-					fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
-						*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
-				}
-				return
-
-			default:
-				fmt.Printf("Ignored event: %s\n", ev)
-			}
-		}
-	}()
-	time.Sleep(60 * time.Second)
+       time.Sleep(60 * time.Second)
        for  {
 	       t := time.Now().Format(time.RFC3339Nano)
 		sensor := SensorReading{
@@ -62,14 +42,23 @@ func main() {
 		}
 		b, err := json.Marshal(sensor)
 		if err != nil {
-			fmt.Println("error:", err)
-		}	
-        	fmt.Printf("Delivering: %v\n", string(b)) 
-		p.ProduceChannel() <- &kafka.Message{TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny}, Value: []byte(b)}
+			fmt.Printf("error:", err)
+		} else {
+        		fmt.Printf("Delivering: %v\n", string(b)) 
+			deliveryChan := make(chan kafka.Event)
+			err = p.Produce(&kafka.Message{TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny}, Value: []byte(b)}, deliveryChan)
+			e := <-deliveryChan
+			m := e.(*kafka.Message)
+
+			if m.TopicPartition.Error != nil {
+				fmt.Printf("Delivery failed: %v\n", m.TopicPartition.Error)
+			} else {
+				fmt.Printf("Delivered message to topic %s [%d] at offset %v\n",
+				*m.TopicPartition.Topic, m.TopicPartition.Partition, m.TopicPartition.Offset)
+			}
+		}
 	}
 	// wait for delivery report goroutine to finish
-	_ = <-doneChan
-
 	p.Close()
 
 	// Wait for message deliveries before shutting down
