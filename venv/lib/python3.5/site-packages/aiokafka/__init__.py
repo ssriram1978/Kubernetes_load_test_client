d97cb4c20bada383e4ceab7b80d2c5151d91dcb8
sriram sridhar <sriramsridharjob78@gmail.com>
2019-07-10 10:58:19 -0400

    Had to merge the confluent kafka go files manually into the docker.

---
+++ venv/lib/python3.5/site-packages/aiokafka/__init__.py
@@ -0,0 +1,27 @@
+__version__ = '0.5.2'  # noqa
+
+from .abc import ConsumerRebalanceListener
+from .client import AIOKafkaClient
+from .consumer import AIOKafkaConsumer
+from .errors import ConsumerStoppedError, IllegalOperation
+from .producer import AIOKafkaProducer
+from .structs import (
+    TopicPartition, ConsumerRecord, OffsetAndTimestamp, OffsetAndMetadata
+)
+from .util import PY_35, ensure_future
+
+
+__all__ = [
+    # Clients API
+    "AIOKafkaProducer",
+    "AIOKafkaConsumer",
+    # ABC's
+    "ConsumerRebalanceListener",
+    # Errors
+    "ConsumerStoppedError", "IllegalOperation",
+    # Structs
+    "ConsumerRecord", "TopicPartition", "OffsetAndTimestamp",
+    "OffsetAndMetadata"
+]
+
+(PY_35, ensure_future, AIOKafkaClient)
